456e683bd69bd5024906f135e529942de1edf3a4
nconrad <1194246+nconrad@users.noreply.github.com>
2024-01-18 22:35:58 -0600

    add sage beehive request example

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<string[]>()
+
+  useEffect(() => {
+    const query = {start: "-1m", filter: {vsn: "W097"}}
+    fetch("https://data.sagecontinuum.org/api/v1/query", {
+        method: 'POST',
+        body: JSON.stringify(query)
+    }).then(res => res.text())
+      .then(text => setData(text.split('\n')))
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ul>
+      {data?.map(record => <li>{record}</li>)}
+    </ul>
   )
 }
 
